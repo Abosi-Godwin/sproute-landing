6f0cd9d510fca27254c7fbbf7244ffeead09de27
Abosi-Godwin <bbnl6060@github.com>
2026-06-23 09:36:28 +0100

    improve the security

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,34 @@
- 
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: [],
+        domains: []
     },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff"
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY"
+                    },
+                    {
+                        key: "X-XSS-Protection",
+                        value: "1; mode=block"
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=63072000; includeSubDomains; preload"
+                    }
+                ]
+            }
+        ];
+    }
 };
 
 export default nextConfig;
